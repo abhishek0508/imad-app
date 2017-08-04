12ca9a56658bdae4037cb48a57333e6d6f2d4631
abhishek0508 <abhishekcool0508@gmail.com>
2017-08-05 00:39:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articleone', function(req,res){
+app.get('/article-one', function(req,res){
     res.send("Article one requested will be served here");
 });
-app.get('/articletwo', function(req,res){
+app.get('/article-two', function(req,res){
     res.send("Article two requested will be served here");
 });
-app.get('/articlethree', function(req,res){
+app.get('/article-three', function(req,res){
     res.send("Article three requested will be served here");
 });
 
